9990c4238975cdbb82ba67b9baa923443c14cad8
Nel <nel.renaudin@gmail.com>
2022-09-24 10:44:58 +0200

    Add Book constructor and function to add book from user inputs to library array

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const library = [];
 
-function Book() {
-
+function Book(title, author, pages, readYet) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readYet = readYet ? readYet : false;
 }
 
 function addBookToLibrary() {
-  
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const readYet = document.querySelector('#read-yet').checked;
+
+  const newBook = new Book(title, author, pages, readYet);
+
+  library.push(newBook);
 }
+
+document.querySelector('#add-book-form').addEventListener('submit', event => {
+  event.preventDefault();
+  addBookToLibrary();
+});
